6f2c4d43f69972e4d25a404a9dd70a226082d8dc
Suly <sparedes828@gmail.com>
2016-11-28 12:47:49 -0500

    Added basic Tone code

---
+++ Speaker_V1.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+tone(9,146.8);
+delay(100);
+
+tone(9,138);
+delay(500);
+
+tone(9,164.8);
+delay(500);
+
+tone(9,146.8);
+delay(500);
+
+tone(9,196);
+delay(500);
+
+tone(9,185);
+delay(500);
+
+
+noTone(9);
+delay(200);
+}
